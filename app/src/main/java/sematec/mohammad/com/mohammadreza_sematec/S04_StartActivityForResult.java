ad83f41efeeee709b01c2ca34146100efe8d294f
MohammadReza <Sarani.MohammadReza@gmail.com>
2019-02-06 14:57:09 +0330

    S04--Hawk

---
+++ app/src/main/java/sematec/mohammad/com/mohammadreza_sematec/S04_StartActivityForResult.java
@@ -0,0 +1,155 @@
+package sematec.mohammad.com.mohammadreza_sematec;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.orhanobut.hawk.Hawk;
+
+import sematec.mohammad.com.mohammadreza_sematec.Application.App;
+import sematec.mohammad.com.mohammadreza_sematec.Application.Application;
+
+public class S04_StartActivityForResult extends AppCompatActivity implements View.OnClickListener , TextWatcher {
+
+    EditText ext_FirstName,ext_LastName,ext_Phone,ext_Mail,ext_Age;
+
+    Button   btn_Login;
+
+    String   FirstName ,LastName   ,Age   ,Phone   ,Email;
+
+    String   Hawk_First_Name,Hawk_LastName,Hawk_Age,Hawk_Email,Hawk_Phone;
+
+    Intent   intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.s04__start_activity_for_result);
+
+
+
+
+
+        init();
+        Listener();
+    }
+
+
+    private void init() {
+
+        ext_FirstName   =   findViewById(R.id.FirstName);
+        ext_LastName    =   findViewById(R.id.LastName);
+        ext_Age         =   findViewById(R.id.Age);
+        ext_Mail        =   findViewById(R.id.Mail);
+        ext_Phone       =   findViewById(R.id.Phone);
+
+        btn_Login       =   findViewById(R.id.Login);
+
+
+        Hawk.init(Application.getContext()).build();
+
+
+        intent          =   new Intent( this   ,S04_StartActivityForResult2.class  );
+
+
+
+        Hawk_init();
+
+    }
+
+    private void Hawk_init() {
+
+   try {
+       Hawk_First_Name = Hawk.get(S04_StartActivityForResult2.TAG_FIRSTNAME_SHARED);
+       Hawk_LastName = Hawk.get(S04_StartActivityForResult2.TAG_LASTNAME_SHARED);
+       Hawk_Age = Hawk.get(S04_StartActivityForResult2.TAG_AGE_SHARED);
+       Hawk_Email = Hawk.get(S04_StartActivityForResult2.TAG_EMAIL_SHARED);
+       Hawk_Phone = Hawk.get(S04_StartActivityForResult2.TAG_PHONE_SHARED);
+
+
+       ext_FirstName.setText(Hawk_First_Name);
+       ext_LastName.setText(Hawk_LastName);
+       ext_Age.setText(Hawk_Age);
+       ext_Mail.setText(Hawk_Email);
+       ext_Phone.setText(Hawk_Phone);
+   }catch (Exception e ){
+       e.printStackTrace();
+   }
+
+    }
+
+    private void Listener() {
+        btn_Login.setOnClickListener(this);
+
+
+        ext_FirstName.addTextChangedListener(this);
+        ext_LastName.addTextChangedListener(this);
+        ext_Age.addTextChangedListener(this);
+        ext_Mail.addTextChangedListener(this);
+        ext_Phone.addTextChangedListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.Login:
+                try {
+                    if (FirstName.length() > 3 && LastName.length() > 5 && Age.length() >= 1 && Email.length() > 8 && Phone.length() > 10) {
+
+                        intent.putExtra("FirstName", FirstName);
+                        intent.putExtra("LastName", LastName);
+                        intent.putExtra("Age", Age);
+                        intent.putExtra("Email", Email);
+                        intent.putExtra("Phone", Phone);
+
+                        startActivityForResult(intent,20);
+                    } else App.T_Short("Please Complete All Field And Try Again...");
+                }catch (Exception e ){
+                    e.printStackTrace();
+                    App.T_Short("Please Complete All Field And Try Again...  " + e);
+                }
+
+                break;
+        }
+
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+
+        FirstName       =   ext_FirstName.getText().toString().trim();
+        LastName        =   ext_LastName.getText().toString().trim();
+        Age             =   ext_Age.getText().toString().trim();
+        Email           =   ext_Mail.getText().toString().trim();
+        Phone           =   ext_Phone.getText().toString().trim();
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ app/src/main/java/sematec/mohammad/com/mohammadreza_sematec/S04_StartActivityForResult2.java
@@ -0,0 +1,117 @@
+package sematec.mohammad.com.mohammadreza_sematec;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.orhanobut.hawk.Hawk;
+
+import java.util.concurrent.ExecutionException;
+
+import sematec.mohammad.com.mohammadreza_sematec.Application.App;
+import sematec.mohammad.com.mohammadreza_sematec.Application.Application;
+import sematec.mohammad.com.mohammadreza_sematec.R;
+
+public class S04_StartActivityForResult2 extends AppCompatActivity implements View.OnClickListener {
+
+
+    TextView txt_FirstName, txt_LastName, txt_Age, txt_Email, txt_Phone;
+
+    public final String Tag_FirstName = "FirstName";
+    public final String Tag_LastName = "LastName";
+    public final String Tag_Age = "Age";
+    public final String Tag_Email = "Email";
+    public final String Tag_Phone = "Phone";
+
+
+    public static final String TAG_FIRSTNAME_SHARED = "FirstName";
+    public static final String TAG_LASTNAME_SHARED = "LastName";
+    public static final String TAG_AGE_SHARED = "AGE";
+    public static final String TAG_EMAIL_SHARED = "Email";
+    public static final String TAG_PHONE_SHARED = "Phone";
+
+    String FirstName, LastName, Age, Email, Phone;
+
+    Button btn_Verify, btn_Back;
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_s04__start_for_result2);
+
+        init();
+        Listener();
+
+    }
+
+
+    private void init() {
+         intent = getIntent();
+
+        FirstName = intent.getStringExtra(Tag_FirstName);
+        LastName =  intent.getStringExtra(Tag_LastName);
+        Age =       intent.getStringExtra(Tag_Age);
+        Email =     intent.getStringExtra(Tag_Email);
+        Phone =     intent.getStringExtra(Tag_Phone);
+
+
+        txt_FirstName = findViewById(R.id.txt_FirstName);
+        txt_LastName = findViewById(R.id.txt_LastName);
+        txt_Age = findViewById(R.id.txt_Age);
+        txt_Email = findViewById(R.id.txt_Email);
+        txt_Phone = findViewById(R.id.txt_Phone);
+
+        btn_Verify = findViewById(R.id.btn_Verify);
+        btn_Back = findViewById(R.id.btn_Back);
+
+
+        txt_FirstName.setText("FirstName : " + FirstName);
+        txt_LastName.setText("LastName : " + LastName);
+        txt_Age.setText("Age : " + Age);
+        txt_Email.setText("Email : " + Email);
+        txt_Phone.setText("Phone : " + Phone);
+    }
+
+
+    private void Listener() {
+        btn_Verify.setOnClickListener(this);
+        btn_Back.setOnClickListener(this);
+
+        Hawk.init(Application.getContext()).build();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == btn_Verify.getId()) {
+            try {
+                Hawk.put(TAG_FIRSTNAME_SHARED, FirstName);
+                Hawk.put(TAG_LASTNAME_SHARED, LastName);
+                Hawk.put(TAG_AGE_SHARED, Age);
+                Hawk.put(TAG_PHONE_SHARED, Phone);
+                Hawk.put(TAG_EMAIL_SHARED, Email);
+            } catch (Exception execution) {
+                execution.printStackTrace();
+            }
+
+            App.T_Short("Login ....");
+        }
+
+
+        if (v.getId()==btn_Back.getId()){
+
+            setResult(RESULT_OK, intent);
+            finish();
+
+        }
+
+    }
+
+
+}
